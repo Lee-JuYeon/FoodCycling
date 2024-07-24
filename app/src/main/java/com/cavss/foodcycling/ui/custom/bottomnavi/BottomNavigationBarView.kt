f6708a6b7087876a0111e9fb2b26977bea519e0f
cavss <cavss1118@gmail.com>
2024-07-25 06:11:36 +0900

    bottom navigation import update

--- app/src/main/java/com/cavss/foodcycling/ui/custom/bottomnavi/BottomNavigationBarView.kt
+++ app/src/main/java/com/cavss/foodcycling/ui/custom/bottomnavi/BottomNavigationBarView.kt
@@ -8,7 +8,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
-import com.cavss.foodcycling.BottomNavItem
 
 
 @Composable
